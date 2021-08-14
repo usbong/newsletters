f92e245308e61c5154924c3a265bc4ef74e935db
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-08-14 08:16:32 +0800

    Add files via upload

---
+++ usbong_newsletters/application/views/templates/header.php
@@ -0,0 +1,36 @@
+<!--
+  Copyright 2021 SYSON, MICHAEL B.
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+
+  @company: USBONG
+  @author: SYSON, MICHAEL B.
+  @date created: 2020
+  @date updated: 20210814
+  @website address: http://www.usbong.ph
+  
+  Computer Web Browser Address (Example):
+  1) http://localhost/usbong_kms/server/2021/viewNewsletter202106.php   
+  
+  Recommended Reference:
+  1) https://www.w3schools.com/howto/howto_css_table_center.asp;
+  //last accessed: 20210703
+  
+  //TO-DO: -update: this
+-->
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!-- 
+<html lang="en">
+<head>
+</head>
+<body>
+-->
+	<div class="header-banner">
+	</div>
+<!-- 
+</body>
+</html>
+-->
