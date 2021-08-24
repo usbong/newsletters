3754721fee5f6835c8b78747d880b73795016f15
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-08-24 20:38:26 +0800

    Add files via upload

---
+++ usbong_newsletters/server/2021/viewNewsletter202108.php
@@ -0,0 +1,524 @@
+<!--
+  Copyright 2021 SYSON, MICHAEL B.
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+
+  @company: USBONG
+  @author: SYSON, MICHAEL B.
+  @date created: 20210701
+  @date updated: 20210824
+  @website address: http://www.usbong.ph
+
+  Input:
+  1) MySQL Database with Newsletter details
+
+  Output:
+  1) Auto-generated print-ready Newsletter that is viewable on a Computer Web Browser  
+  
+  Computer Web Browser Address (Example):
+  1) http://localhost/usbong_kms/server/viewNewsletter.php   
+  
+  Recommended Reference:
+  1) https://www.w3schools.com/howto/howto_css_table_center.asp;
+  //last accessed: 20210703
+-->
+<?php
+//defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+   <!-- <meta charset="utf-8"> -->
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <!-- Reference: Apache Friends Dashboard index.html -->
+    <!-- "Always force latest IE rendering engine or request Chrome Frame" -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <style type="text/css">
+	/**/
+	                    body
+                        {
+							font-family: Arial;
+							font-size: 12pt;
+							color: rgb(120,63,4);
+
+							/* This makes the width of the output page that is displayed on a browser equal with that of the printed page. */
+							/* Legal Size; Landscape*/							
+
+							width: 670px;/*860px;*//* 802px;*//* 670px */
+
+							/* removed by Mike, 20210105 */
+							/* use zoom 67% scale*/
+							/* at present, command not support in Mozilla Firefox */				
+/*							zoom: 67%; 
+	
+							transform: scale(0.67);
+							transform-origin: 0 0;	
+*/							
+                        }
+												
+						div.formTitle
+						{
+							text-align: right;
+/*							font-weight: bold;*/	
+							font-size: 12pt;
+							color: rgb(0,0,0); /* black */
+						}
+						
+						div.newsletterTitle
+						{
+							text-align: center;
+							font-size: 16pt;
+							font-weight: bold;
+						}
+
+						div.newsletterSubTitle
+						{
+							text-align: center;
+							font-size: 12pt;
+							color: #222222; /* rgb(0,0,0); //black */
+							font-weight: bold;
+						}
+						
+						div.newsletterDate
+						{
+							text-align: right;
+							font-size: 12pt;
+							font-weight: bold;
+							color: #cd580f /* #e69138 orange */
+						}
+
+						div.copyright
+						{
+							text-align: center;
+							font-size: 10pt;
+							color: rgb(0,0,0); /* black */
+						}
+					
+						h3
+						{
+							color: rgb(0,0,0); /* black */							
+						}
+					
+						h4
+						{
+							color: rgb(64,64,64); /* from black to white */							
+						}
+
+						h5
+						{							
+							color: rgb(85,43,0);	
+						}
+						
+						b
+						{
+							color: rgb(0,0,0); /* from black to white */ 							
+						}
+
+						img.Image-companyLogo {
+							max-width: 60%;
+							height: auto;
+							float: left;
+							text-align: center;
+							padding-left: 20px;
+							padding-top: 10px;
+						}
+
+						img.Image-moscLogo {
+							max-width: 20%;
+							height: auto;
+							float: left;
+							text-align: center;
+						}
+						
+						img.newsletterImage {
+							max-width: 100%;
+							height: auto;
+							border: 1px solid #ab9c7d;
+						}
+
+						img.noteImage {
+							max-width: 100%;
+							height: auto;
+							border: 1px solid #ab9c7d;
+						}
+						
+						div.newsletterImageTextDiv {
+							text-align: center;							
+							font-weight: bold;
+						}
+						
+						table.imageTable
+						{
+							width: 100%;
+						}						
+
+						td.imageTableColumn
+						{
+							border: 1px solid #ab9c7d;
+						}
+
+						table.noteTable
+						{
+							border: 1px solid #ab9c7d;
+							width: 90%;
+							margin: 0px;
+							padding: 0px;
+						}
+
+						td.noteTableImageColumn
+						{
+							width:10%;
+							vertical-align: top;
+						}
+				
+						td.noteTableTextColumn
+						{
+							width:100%;
+							text-align: left;							
+							float: left;
+							margin:0;
+							padding:0;
+							display: inline-block;
+							font-weight: bold;							
+							font-size: 11pt;
+							border: 1px solid #ab9c7d;							
+						}
+				
+						table.tablePart2
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						table.tablePart3
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}	
+
+						a.webServiceLink
+						{
+							color: rgb(51,145,23);
+							font-weight: bold;							
+						}
+						
+						a.menuLink
+						{
+							color: rgb(64,64,64);
+							text-decoration: none;
+						}
+
+						a.menuLink:hover
+						{
+							color: rgb(64,64,64);
+							text-decoration: underline;
+						}
+
+						table.menuTable
+						{
+							width: 80%;
+							border: 1px solid rgb(146,208,80);
+							border-collapse: collapse;							 
+							font-size: 10pt;
+							/* Commands to Center-Align Table */
+							margin-left: auto;
+							margin-right: auto;
+						}
+
+						tr.menuRow
+						{
+							background-color: rgb(146,208,80);
+							text-align: center;							
+						}
+						
+						td.menuColumn
+						{
+							background-color: rgb(146,208,80);
+							text-align: center;							
+						}
+
+						td.menuColumn:hover
+						{
+							background-color: rgb(121,176,62);
+							text-align: center;							
+						}
+
+						tr.rowEvenNumber {
+							background-color: #dddddd; <!--#dddddd; = gray #95b3d7; = sky blue; use as row background color-->
+							border: 1pt solid #00ff00;		
+						}
+
+						td.tableHeaderColumnPart1
+						{
+							text-align: left;
+/*							font-weight: bold;
+*/
+						}						
+
+						td.tableHeaderColumnPart2
+						{
+							border: 1px solid #000000;		
+							text-align: center;
+/*							font-weight: bold;
+*/
+						}						
+
+						td.tableHeaderColumnPart3dot1
+						{
+							text-align: left;
+/*							font-weight: bold;
+*/
+							width: 70%;							
+						}						
+
+						td.tableHeaderColumnPart3dot2
+						{
+							text-align: left;
+/*							font-weight: bold;
+*/
+							width: 30%;							
+						}						
+
+
+						td.column
+						{
+							border: 1px dotted #000000;		
+							text-align: center;
+						}						
+
+						td.columnFee
+						{
+							border: 1px dotted #000000;		
+							text-align: right;
+						}
+
+						td.columnFeeTotal
+						{
+							border: 1px dotted #000000;		
+							text-align: right;
+							font-size: 14pt;
+							font-weight: bold;
+						}
+						
+
+						td.columnBorderBottom
+						{
+							border: 1px dotted #ab9c7d;		
+							border-bottom: 4px double black;
+							text-align: center;
+						}						
+
+						td.columnBorderBottomDotted
+						{
+							border: 1px dotted #ab9c7d;		
+							border-bottom: 2px dotted black;
+							text-align: center;
+						}						
+
+						td.columnBorderTopBottom
+						{
+							border: 1px dotted #ab9c7d;		
+							border-top: 2px solid black;
+							border-bottom: 4px double black;
+							text-align: center;
+						}						
+						
+						td.imageColumn
+						{
+							width: 40%;
+							display: inline-block;
+						}						
+
+						td.pageNameColumn
+						{
+							width: 50%;
+							display: inline-block;
+							text-align: right;
+						}												
+    /**/
+    </style>
+    <title>
+      USBONG Newsletter
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css">
+    </style>
+	<base href="http://localhost/usbong_newsletters/" target="_blank">
+  </head>
+	  <script>
+	  </script>
+  <body>
+  
+<?php
+	date_default_timezone_set('Asia/Hong_Kong');
+	
+	//edited by Mike, 20200726
+	//$dateToday = (new DateTime())->format('Y-m-d');
+	$dateToday = Date('Y-m-d');
+
+	// connect to the database
+	//edited by Mike, 20210813
+//	include('usbong-kms-connect.php');
+
+	//Reference: https://stackoverflow.com/questions/16048818/include-php-files-when-they-are-in-different-folders;
+	//answer by: AbsoluteƵERØ, 20130416T2355
+	//edited: 20171021T0640	
+		
+	//echo $_SERVER['DOCUMENT_ROOT']; //example output: C:/xampp/htdocs
+	include($_SERVER['DOCUMENT_ROOT'].'/usbong_newsletters/server/usbong-kms-connect.php');
+
+	$mysqli->set_charset("utf8");
+
+	//TO-DO: -update: this
+
+?>
+	<table class="imageTable">
+	  <tr>
+		<td class="imageColumn">				
+			<img class="Image-companyLogo" src="assets/images/usbongLogo.png">
+		</td>	
+		<td class="pageNameColumn">
+			<div class="formTitle">
+				<b>USBONG NEWSLETTER</b>
+			</div>		
+		</td>
+	</table>
+	<br/>
+<!--	<b>DATE:</b><?php echo " ".$dateToday; ?>
+-->
+	
+	<!-- menu bar -->
+	<table class="menuTable">
+	  <tr class="menuRow">
+		<td class="menuColumn">				
+			<a class="menuLink" href="https://creativecommons.org/licenses/by/4.0/">		
+				Computer
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" href="https://creativecommons.org/licenses/by/4.0/">		
+				Services
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" href="https://creativecommons.org/licenses/by/4.0/">		
+				Team
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" href="https://creativecommons.org/licenses/by/4.0/">		
+				Partners
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" href="https://creativecommons.org/licenses/by/4.0/">		
+				Open Source
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" href="https://creativecommons.org/licenses/by/4.0/">		
+				EXCEL
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" href="https://creativecommons.org/licenses/by/4.0/">		
+				Contact
+			</a>
+		</td>
+	</table>
+
+	<br>
+	<br>	
+	<div class="newsletterTitle">
+		↑ KNOW-HOW causes<br/>
+		↑ available PRODUCTS・SERVICES<br/>
+	</div>
+	<br>
+	<div class="newsletterSubTitle">
+		PHILIPPINES: USBONG CONTINUES TO PROVIDE<br/>
+		COMPUTER SERVICE ASSISTANCE<br/>
+		BY SHARING KNOW-HOW ON MAPARAANG PAGBIBILANG<br/>
+	</div>
+	<br>	
+	<div class="newsletterDate">
+	AUGUST  2021
+	</div>
+	<br>
+	<br>
+
+To eliminate excess steps and inefficient time usage, Marikina Orthopedic Specialty Clinic (MOSC) requested Usbong's services to automate routine, monotonous tasks and share with unit members know-how on computers.
+
+<h3>1) Continued to update Knowledge Management System (KMS)</h3>
+<h4>1.1) continued: to share know-how on computers and Information Technology (IT) tools</h4>
+<h5>1.1.1) shared: answers to questions via <a class="webServiceLink" href="http://www.stackoverflow.com" target="_blank">www.stackoverflow.com</a>, <a class="webServiceLink" href="http://www.askubuntu.com" target="_blank">www.askubuntu.com</a>, et cetera<br/>
+--> reminder: There exists logical answer to logical question;<br/>
+--> where: logical = clear, not emotional<br/>
+--> where: logical questions = mga tanong ukol sa Maparaang Pagbibilang.<br/>
+--> Example Question: How to execute <a class="webServiceLink" href="http://https://stackoverflow.com/questions/46258919/drawing-circle-with-gl-polygon-radius-out-of-scale" target="_blank">"Drawing circle with GL_POLYGON, radius out of scale"</a>?<br/>
+--> Example Answer (executed on <a class="webServiceLink" href="http://www.lubuntu.me">LUBUNTU (LTS 20.04)</a> machine):<br/>
+--> Example OUTPUT:</h5>
+<table>
+	<tr>
+		<td>
+			<div class="newsletterImageTextDiv">
+			<img class="newsletterImage" width="60%" height="60%" src="assets/images/newsletter/202108/drawCircleExample.jpg"><br/>
+				Auto-drawn Circle Shape
+			</div>
+		</td>
+	</tr>
+</table>
+<br/>
+<!-- TO-DO: -add: stackoverflow answer to Rabbid76's answer
+<table>
+	<tr>
+		<td>
+			<div class="newsletterImageTextDiv">
+			<img class="newsletterImage" width="60%" height="60%" src="assets/images/newsletter/202108/drawCircleExample.jpg"><br/>
+				Auto-drawn Circle Shape
+			</div>
+		</td>
+	</tr>
+</table>
+-->
+<h5>1.1.2) shared: computer instructions to climb diagonal of right triangle tile on <a class="webServiceLink" href="http://www.github.com/usbong/story">www.github.com</a>
+</h5>
+<b>NOTE (from SYSON, MICHAEL B.):</b>
+<table class="noteTable">
+	<tr>
+		<td class="noteTableImageColumn">
+			<img class="noteImage" src="assets/images/Syson,MichaelB.png">
+		</td>
+		<td class="noteTableTextColumn">
+			We need to find the correct position in the Hypothenous, i.e. diagonal line of the right triangle. We know the following:<br/>
+			<br/>
+			1) right triangle, i.e. has 90degrees angle inside;<br/>
+			--> There exist 3 angle values in a triangle; added total is 180degrees<br/>
+			--> In this case, the two (2) remaining angle degrees vales: 45 and 45;<br/>
+			--> from 90/2=45;<br/>
+			<br/>
+		</td>
+	</tr>
+</table>
+
+<!-- TO-DO: -update: this -->
+
+	<br>
+	<br>
+	<br />
+	<div class="copyright">
+		<span>
+			© SYSON, MICHAEL B. & USBONG. 2011~2021. All rights reserved.<br/>
+			<a href="https://creativecommons.org/licenses/by/4.0/">
+				Creative Commons Attribution License BY 4.0
+			</a>
+		</span>
+	</div>		 
+  </body>
+</html>
