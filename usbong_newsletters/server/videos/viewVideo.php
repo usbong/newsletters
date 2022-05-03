50860c34c32218edda8dc0793b19b3a016b39490
Masarap Mabuhay <masarapmabuhay@gmail.com>
2022-05-03 19:59:11 +0800

    Add files via upload

--- usbong_newsletters/server/videos/viewVideo.php
+++ usbong_newsletters/server/videos/viewVideo.php
@@ -1,5 +1,5 @@
 <!--
-  Copyright 2021 SYSON, MICHAEL B.
+  Copyright 2021~2022 SYSON, MICHAEL B.
   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
   http://www.apache.org/licenses/LICENSE-2.0
   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
@@ -7,7 +7,7 @@
   @company: USBONG
   @author: SYSON, MICHAEL B.
   @date created: 20220102
-  @date updated: 20220103
+  @date updated: 20220503; from 20220103
   @website address: http://www.usbong.ph
     
   Input:
@@ -494,7 +494,7 @@
 	
 	<div class="copyright">
 		<span>
-			© SYSON, MICHAEL B. & USBONG. 2011~2021. All rights reserved.<br/>
+			© SYSON, MICHAEL B. & USBONG. 2011~<?php echo date("Y");?>. All rights reserved.<br/>
 			<a class="webServiceLink" target="_blank" href="https://creativecommons.org/licenses/by/4.0/">
 				Creative Commons Attribution License BY 4.0
 			</a>
