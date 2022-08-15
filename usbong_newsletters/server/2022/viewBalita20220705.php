818b048836c5bdbe638c0de90a6a1d592637765c
Masarap Mabuhay <masarapmabuhay@gmail.com>
2022-08-15 20:03:00 +0800

    Add files via upload

---
+++ usbong_newsletters/server/2022/viewBalita20220705.php
@@ -0,0 +1,85 @@
+<!--
+  Copyright 2021~2022 SYSON, MICHAEL B. 
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+  
+  @company: USBONG
+  @author: SYSON, MICHAEL B.
+  @date created: 20211011
+  @date updated: 20220815; from 20220705
+  @website address: http://www.usbong.ph
+ 
+  Input:
+  1) Lessons-learned HTML TEXT FILE
+  Output:
+  1) Usbong Balita viewable using Computer/Mobile Web Browsers  
+  
+  Computer Browser: Website Address (Example):
+  1) local: http://localhost/usbong_newsletters/server/usbongBalita.php
+  2) public internet: http://store.usbong.ph/server/usbongBalita.php
+  
+  Recommended References:
+  1) https://www.w3schools.com/howto/howto_css_table_center.asp;
+  //last accessed: 20210703
+  2) https://www.asahi.com/; last accessed: 20220626
+  
+  3) https://www.1101.com/home.html; last accessed: 20220626
+  
+  4) AMAZON.COM Kindle eBOOK Reader
+  //TO-DO: -add: mobile responsiveness via reusing USBONG STORE computer instructions received from ZENT as paid sub-contractor  
+  reminder: pay: to learn technique for USBONG to share lessons-learned with PUBLIC
+  
+  //TO-DO: -update: newsletter pages system to be more organized  
+-->
+
+<h3>
+<a class="webServiceLink" target="_blank" href="https://www.asahi.com/articles/DA3S15330231.html?iref=comtop_Opinion_05">
+	「（天声人語）書店消失」</a> 
+</h3>				
+
+<div class="textDoubleSpacedDiv">
+
+<!-- TO-DO: -add: as NOTE	
+last accessed: 20220622<br/>	
+07:14~07:37; Ginugol na Panahon: 23MIN~<br/>
+-->
+
+<blockquote class="usbongBlockquote">
+<p>
+（天声人語）書店消失
+</p>
+</blockquote>
+(Tinig ng Langit; Salaysay ng Tao)<br/>
+Pagkawala ng mga Tindahan ng Aklat<br/>
+<br/>
+<blockquote class="usbongBlockquote">
+<p>
+    有料会員記事
+</p>
+</blockquote>
+Sulatin sa nagbabayad na kasapi<br/>
+<br/>
+<blockquote class="usbongBlockquote">
+<p>
+    2022年6月21日 5時00分
+</p>
+</blockquote>
+2022-06-21T05:00<br/>
+<br/>
+<blockquote class="usbongBlockquote">
+<p>
+夢に本屋が出てきたことがある。書棚で何かを探しているのだが、そこはむかし郷里にあった小さな店なのだ。子どもの頃から通い、マンガ雑誌の発売日に駆け込んでいた。文庫本の棚では星新一の面白さに出会った▼自分のなかでは本屋の原型なのかもしれない。これからの子どもたちには縁のない話になるだろうか。身の回りか…
+</p>
+</blockquote>
+    
+May lumabas na Tindahan ng Aklat sa aking panaginip. Isa itong maliit na tindahan na naroon noon sa pook ng aking kinalakihan; May hinahanap akong kung ano sa lagayan ng mga aklat. Simula pa noong dumaan ako sa aking kabataan, hinahabol ko na ang araw ng bentahan ng Magasin ng Komiks (MANGA). May natagpuan akong nakaaaliw kay SEI SHINICHI (Manunulat; Bituin, Bagong Isa) sa lagayan ng mga aklat ng malaking lalagyan ng mga sulatin. Sa loob-loob ko, marahil ito ang pinagmulang hugis ng Tindahan ng Aklat. Mawawala kaya ang usaping ganito sa mga kabataan mula ngayon? Sa paligid...
+<br/>
+<br/>
+<blockquote class="usbongBlockquote">
+<p>
+    この記事は有料会員記事です。残り454文字有料会員になると続きをお読みいただけます。
+</p>
+</blockquote>
+Sa nagbabayad na kasapi ang sulating ito. May natitira pang 454 na titik. Kapag naging nagbabayad na kasapi na po kayo, maaari niyo na pong mabasa ang kasunod.
+</div>
